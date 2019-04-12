16a89a9dfe8351f001f2ec31fd296e9288c7f66b
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 05:09:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
-function theBeetlesPlay(musicians,instruments){
+function theBeatlesPlay(musicians,instruments){
   var combination = [];
   for(let i=0; i<musicians.length; i++){
     combination[i]=musicians[i].concat(" plays ").concat(instruments[i])
